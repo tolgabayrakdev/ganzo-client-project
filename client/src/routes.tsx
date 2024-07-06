54ddd309b20f5162bd42dc9b43922c47230742aa
tolgabayrakdev <bayraktolga28@gmail.com>
2024-07-06 16:53:08 +0300

    setup

---
+++ client/src/routes.tsx
@@ -0,0 +1,15 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+
+const HomePage = lazy(() => import("./pages/Home"));
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />
+
+    }
+]);
+
+export default routes;
